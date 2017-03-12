dbae33844031c3f62e403640812d1f5ae286a0d4
Artem Aleksandruk <artem.alek@icloud.com>
2017-03-11 21:19:36 -0500

    back to initial commit, something went wrong and starting over.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 // Require your controllers here
-const Linkshare = require('../controllers/Linkshare');
+
 
 module.exports = (app) => {
   // Add your routes here
-    app.post('/users', Linkshare.create())
+    app.post('/users', UserController.register);
 };
